deeaa15860e764b24d421fca8d08024e05be0376
david lizhong huang <davidlizhonghuang@users.noreply.github.com>
2022-08-12 08:57:12 +1000

    first code

---
+++ consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿ 
+using System.Text;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+
+var factory=new ConnectionFactory{
+Uri=new Uri("amqp://guest:guest@localhost:5672")
+
+};
+
+using var connection=factory.CreateConnection();
+using var channel=connection.CreateModel();
+
+channel.QueueDeclare("davidqueue",
+durable:true,
+exclusive:false,
+autoDelete:false,
+arguments:null
+
+);
+
+//5672 is used by internal container docker
+
+var consumer =new EventingBasicConsumer(channel);
+consumer.Received+=(sender,e)=>{
+var body=e.Body.ToArray();
+var message=Encoding.UTF8.GetString(body);
+Console.WriteLine(message);
+};
+
+channel.BasicConsume("davidqueue",true, consumer);
+
+//now consumer is ready
+
+Console.ReadLine();
---
+++ producer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+
+var factory=new ConnectionFactory{
+Uri=new Uri("amqp://davidliz:Archer!!77@localhost:5672")
+
+};
+
+using var connection=factory.CreateConnection();
+using var channel=connection.CreateModel();
+
+channel.QueueDeclare("davidqueue",
+durable:true,
+exclusive:false,
+autoDelete:false,
+arguments:null
+
+);
+
+//5672 is used by internal container docker
+
+var message =new {Name="producer", Message="first message from producer"};
+
+var body=Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+
+channel.BasicPublish("","davidqueue", null, body);
+
+//noe producer is ready to pass body to the queue
